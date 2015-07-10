bb89d431899ce12d4a7f95547d72b54528e849d5
Fabien LOISON <flo@flogisoft.com>
2015-07-10 14:09:31 +0200

    String extraction from javascript source implemented

--- src/extract.js
+++ src/extract.js
@@ -59,15 +59,85 @@ extract.main = function(jsFiles, output, options) {
 
                 function(file, callback) {
                     fs.readFile(file, function(error, data) {
-                        console.log(data.toString());  // FIXME
+                        var strings = extract.extractStrings(data.toString(), options.functions);
+                        console.log(strings);  //FIXME
                     });
                 },
 
                 function() {
+                    // TODO
                 }
             );
         }
     );
+};
+
+/**
+ * Extracts strings from the given Javascript source code.
+ *
+ * @method extractStrings
+ * @static
+ * @param {String} source The Javascript source code.
+ * @param {Array} functionsNames The name of th etranslation functions to search in the source.
+ * @return {Object} Translatable strings `{ <string>: [<lines>] }`.
+ */
+extract.extractStrings = function(source, functionsNames) {
+    var strings = {};
+    var ast = espree.parse(source, {tolerant: true, tokens: true, loc: true});
+
+    function _cleanString(str) {
+        var quote = str[0];
+        str = str.slice(1, str.length-1);  // Remove quotes
+        str = (quote == "'") ? str.replace(/\\'/g, "'") : str.replace(/\\"/g, '"');  // Unescape quotes
+        str = str.replace(/\\(\r?\n)/g, "$1");  // Unescape EOL
+        return str;
+    }
+
+    var f_fn = false;  // In function flag
+    var f_sp = false;  // In parenthesis flag
+    var buff = true;   // Buff to concat splitted strings
+    for (var i=0 ; i<ast.tokens.length ; i++) {
+
+        // ?
+        if (!f_fn && !f_sp) {
+            if (ast.tokens[i].type == "Identifier" && functionsNames.indexOf(ast.tokens[i].value) > -1) {
+                f_fn = true;
+            }
+        }
+
+        // functionName
+        else if (f_fn && !f_sp) {
+            if (ast.tokens[i].type == "Punctuator" && ast.tokens[i].value == "(") {
+                f_sp = true;
+                buff = "";
+            }
+            else {
+                f_fn = false;
+            }
+        }
+
+        // functionName (
+        else if (f_fn && f_sp) {
+            if (ast.tokens[i].type == "String") {
+                buff += _cleanString(ast.tokens[i].value);
+            }
+            else if (ast.tokens[i].type == "Punctuator" && ast.tokens[i].value == "+") {
+                continue;
+            }
+            else {
+                if (strings[buff] === undefined) {
+                    strings[buff] = [ast.tokens[i].loc.start.line];
+                }
+                else {
+                    strings[buff].push(ast.tokens[i].loc.start.line);
+                }
+                f_fn = false;
+                f_sp = false;
+            }
+        }
+    }
+
+    return strings;
 };
 
 
--- test/files/example.js
+++ test/files/example.js
@@ -4,7 +4,7 @@
  */
 var text0 = _("This is a translatable string");
 var text1 = _('This is a other\' translatable string');
-var text2 = "This is NOT a stranslatable string");
+var text2 = "This is NOT a stranslatable string";
 var text3 = _("blah blah" /* Hello */ + " World");
 
 _("This is a long" +
@@ -12,16 +12,25 @@ _("This is a long" +
 
 _("This is an other long " +
   // comment.....
-  "translatable string ");
+  "translatable string");
+
+_(
+"This is a bad \
+multiline string"
+);
 
 // _("This should not be translated")
 
 var username = "Jean-Kevin";
 _("Hello {username}, how are you today?", {"username": username});
 
-var text42 = _("This is a translatable string");
 
 Stone.gettext("THIS SHOULD BE TRANSLATABLE TOO!");
 Stone.lazyGettext("THIS SHOULD BE TRANSLATABLE TOO! #Lazy");
 
+_ ( "Spaces everywhere" ) ;
+
 _("Chaîne avec des caractères spéciaux");
+
+_("duplicated straing");
+_("duplicated straing");
